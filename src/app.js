a8ac28a5caa7976697b04854b2ca1040f40ef842
kardachh <kardakov.denis49@gmail.com>
2023-01-23 22:10:10 +0300

    reviews block

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 console.log("test")
+const swiper = new Swiper('.swiper', {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+                nextEl: `.review-next`,
+                prevEl: `.review-prev`,
+              },
+});
+
+console.log(swiper)
